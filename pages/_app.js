8554789bde697cf7e320e29b43219648651ee235
Eunchae Ko <kec0130@gmail.com>
2022-04-07 15:01:37 +0900

    Add ServerSideProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import Layout from "../components/Layout";
-import NavBar from "../components/NavBar";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProp }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProp} />
+      <Component {...pageProps} />
     </Layout>
   );
 }
